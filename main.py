d8c3f2c5470b8ca7b6409452a8a0404d4e91aad2
nao-riku <76162238+nao-riku@users.noreply.github.com>
2023-10-19 11:50:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def login_twitter(account, password, tel_, driver):
                     act.perform()
                 time.sleep(2) 
                 driver.get_screenshot_as_file("c.png")
-                driver.find_elements(By.CSS_SELECTOR, "[role=button]")[2].click()
+                driver.find_element(By.CSS_SELECTOR, "[data-testid=ocfEnterTextNextButton]").click()
                 time.sleep(10)
             else:
                 driver.get_screenshot_as_file("c.png")
